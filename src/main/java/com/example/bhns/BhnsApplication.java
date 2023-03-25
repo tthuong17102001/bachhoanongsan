a4cf5b58a7ca1484b9cec8d98ca44c2bafe33d27
Admin <aea4badao1@gmail.com>
2023-03-25 20:50:02 +0700

    create project

---
+++ src/main/java/com/example/bhns/BhnsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bhns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bhns/BhnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bhns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
